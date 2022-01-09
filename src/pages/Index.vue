b9f1e64799945c467d1142e6cd9038b414ab2e99
Por-045 <6210110045@psu.ac.th>
2022-01-09 13:59:23 +0700

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="q-pa-md">
+    <q-carousel
+      animated
+      v-model="slide"
+      arrows
+      navigation
+      infinite
+    >
+      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+    </q-carousel>
+  </div>
+  <br>
+  <q-card>
+    <q-btn class="absolute-center" color="primary" icon="home" label="About Me" @click="$router.replace('AboutMe')"/>
+  </q-card>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      slide: ref(1)
+    }
+  }
+}
+</script>
